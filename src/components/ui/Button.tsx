1a0105f7be8f3a0a51ac1727df07e8c8d483a9a2
silentesc <scope-frisk-copy@duck.com>
2026-02-08 00:17:59 +0100

    Add Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,17 @@
-function Button() {
-    return <button>Hello Button</button>
+interface ButtonProps {
+    value: string;
+    type?: "button" | "submit" | "reset";
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function Button({ value, type = "button", onClick = () => { }, ...props }: ButtonProps) {
+    return (
+        <>
+            <button onClick={onClick} type={type} className="w-full border-background-tertiary border-2 p-2 outline-none bg-background-secondary hover:bg-background-tertiary hover:cursor-pointer" {...props}>
+                {value}
+            </button>
+        </>
+    )
 }
 
 export default Button;
