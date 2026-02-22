51322cae31016174b147517b7a7152f55be54cb5
silentesc <scope-frisk-copy@duck.com>
2026-02-22 22:37:46 +0100

    implement loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+function Loading() {
+    return (
+        <>
+            <div className="flex justify-center items-center p-4">
+                <div className="h-14 w-14 animate-spin rounded-full border-4 border-black/40 border-l-primary"></div>
+            </div>
+        </>
+    )
+}
+
+export default Loading;
